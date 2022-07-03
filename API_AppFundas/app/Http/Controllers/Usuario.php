2d5994fcfd1a77ac88e157915ffffc147b219572
Jess Hernandez <20193L001072@utcv.edu.mx>
2022-07-03 18:38:31 -0500

    Creación del modelo Usuario y funciones para editar y eliminar dentro del controlador con sus respectivas rutas

--- API_AppFundas/app/Http/Controllers/Usuario.php
+++ API_AppFundas/app/Http/Controllers/Usuario.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usiario;
 use Illuminate\Http\Request;
 
 class Usuario extends Controller
 {
     //Controlador
+
+    //LISTAR UN REGISTRO EN ESPECIFICO
+    public function show(Usiario $usuario) {
+        return response()->json($usuario);
+    }
+
+    //ACTUALIZAR UN REGISTRO 
+    public function update(Request $request, Usiario $usuario) {
+        $usuario->update($request->all());
+        return response()->json([
+            'res' => 'true',
+            'mensaje' => 'Usuario actualizado correctamente'
+        ], 200);
+    }
+
+    //ELIMINAR UN REGISTRO DE MANERA LÓGICA
+    public function destroy(Usiario $usuario) {
+        $usuario->delete();
+        return response()->json([
+            'res' => 'true',
+            'mensaje' => 'Usuario eliminado correctamente'
+        ], 200);
+    }
+
 }
--- API_AppFundas/app/Models/Usiario.php
+++ API_AppFundas/app/Models/Usiario.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Usiario extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $table = 'Usuarios';
+    protected $primaryKey = 'id';
+
+     protected $fillable = [
+        'id',
+        'nombre',
+        'apellidoP',
+        'apellidoM',
+        'usuario',
+        'contrasena',
+        'correo',
+        'telefono',
+        'direccion',
+        'estado',
+        'municipio'
+    ];
 }
